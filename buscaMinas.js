3f9933e27da8cd069c908f1d1671e0f0632e20ed
Castillo5115 <adriancastillo51.15@gmail.com>
2022-10-04 18:35:05 +0200

    Iniciamos el proyecto

---
+++ buscaMinas.js
@@ -0,0 +1,53 @@
+document.write('<h1 id="titulo">Busca Minas</h1>');
+document.write('<table>');
+document.write('<tr>');
+
+for (let i = 0; i < 5; i++) {
+    document.write('<td></td>');
+}
+
+document.write('</tr>');
+document.write('</table>');
+
+
+
+/* <h1 id="titulo">Busca Minas</h1>
+    <div class="objeto">
+        <table class="tabla">
+            <tr>
+                <td>1</td>
+                <td>2</td>
+                <td>Mina</td>
+                <td>Mina</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Mina</td>
+                <td>3</td>
+                <td>4</td>
+                <td>2</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>2</td>
+                <td>Minas</td>
+                <td>2</td>
+                <td>Mina</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>1</td>
+                <td>2</td>
+                <td>3</td>
+                <td>2</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td>1</td>
+                <td>Mina</td>
+                <td>1</td>
+            </tr>
+        </table>
+    </div> */
