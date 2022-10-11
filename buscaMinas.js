a0571322635e90c81e97669d3b02c010a1b10a82
Castillo5115 <adriancastillo51.15@gmail.com>
2022-10-11 18:26:59 +0200

    Codigo limpio

--- buscaMinas.js
+++ buscaMinas.js
@@ -1,106 +1,73 @@
-let numFilas = prompt('¿Cuantas filas quieres?');
-let numColumnas = prompt('¿Cuantas columnas quieres?');
-let numMinas = prompt('¿Cuantas minas quieres q haiga?');
-
-// Titulo de la página
-document.write('<h1 id="titulo">Busca Minas</h1>');
+let maxFilas = prompt('¿Cuántas filas quieres?');
+let maxColumnas = prompt('¿Cuántas columnas quieres?');
+let numMinas = prompt('¿Cuántas minas quieres introducir?');
 
 
-//Creamos el tablero
+// Creamos el tablero en html
+
 document.write('<table>');
 
-for (let i = 0; i < numFilas; i++) {
-        document.write('<tr>');
-        for (let z = 0; z < numColumnas; z++) {
-            document.write('<td></td>');            
-        }
-        document.write('</tr>');
-}
+for (let i = 0; i < maxFilas; i++) {
+    document.write('<tr>');
 
+    for (let j = 0; j < maxColumnas; j++) {
+        document.write('<td></td>');
+    }
+
+    document.write('</tr>');
+}
 document.write('</table>');
 
+
 // Crear array bidimensional para guardar las minas
-
 let arrayTablero = [];
-
 let contadorMinas = 0;
 
-for(let fila = 0; fila < numFilas; fila++){
+for (let fila = 0; fila < maxFilas; fila++) {
+    arrayTablero[fila] = new Array(maxColumnas);
 
-    for(let columna = 0; columna < numColumnas; columna++){
+    for (let columna = 0; columna < maxColumnas; columna++) {
         arrayTablero[fila][columna] = '';
     }
-
 }
 
-while(contadorMinas < numMinas){
-    posFila = Math.floor(Math.random()*numFilas - 1);
-    posColumna = Math.floor(Math.random()*numColumnas - 1);
-    
-    if (arrayTablero[posFila][posColumna] != "Mina"){
+
+let posFila;
+let posColumna;
+
+
+while (contadorMinas < numMinas) {
+    posFila = Math.floor(Math.random()*maxFilas);
+    posColumna = Math.floor(Math.random()*maxColumnas);
+
+    if (arrayTablero[posFila][posColumna] != 'MINA') {
+        arrayTablero[posFila][posColumna] = 'MINA';
+        contadorMinas++ ;
+    };
+  };
+
+
+let numMinasAlrededor;
+
+for (let fila = 0; fila < maxFilas; fila++) {
+    for (let columna = 0; columna < maxColumnas; columna++) {
+        numMinasAlrededor = 0;
+        if (arrayTablero[fila][columna] != 'MINA'){
+            for (let cFila = fila - 1; cFila < fila + 1; cFila++) {
+                for (let cColumna = columna - 1; cColumna < columna + 1; cColumna++) {
+                    if ((cFila >= 0 && cFila < maxFilas) && (cColumna >= 0 && cColumna < maxColumnas)) {
+                        if (arrayTablero[cFila][cColumna] == 'MINA') {
+                            
+                            numMinasAlrededor++;
+                        }
+                    } 
+                }
+            }
+            arrayTablero[fila][columna] = numMinasAlrededor;
+        }
         
-        arrayTablero[posFila][posColumna] = "Mina";
-        contadorMinas ++;
-    
     }
-    
-    document.write(arrayTablero);
-    console.log(arrayTablero);
 }
+
 
 console.log(arrayTablero);
-console.log(contadorMinas);
-
-let fila = 2;
-let columna = 1;
-let minasAlrededor = 0;
-for (let cFila = fila - 1; cFila < fila  +1; cFila++) {
-    for (let cColumna = columna -1; cColumna <= columna + 1 ; cColumna++) {
-        if(cFila >= 0 || cColumna >= 0 || cFila < numFilas || cColumna < numColumnas){
-            if(arraytablero[cFila][cColumna] == "Mina"){
-                minasAlerededor++;
-            }
-        }        
-    }    
-}
-
-/* <h1 id="titulo">Busca Minas</h1>
-    <div class="objeto">
-        <table class="tabla">
-            <tr>
-                <td>1</td>
-                <td>2</td>
-                <td>Mina</td>
-                <td>Mina</td>
-                <td>1</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Mina</td>
-                <td>3</td>
-                <td>4</td>
-                <td>2</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>2</td>
-                <td>Minas</td>
-                <td>2</td>
-                <td>Mina</td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>1</td>
-                <td>2</td>
-                <td>3</td>
-                <td>2</td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td>1</td>
-                <td>Mina</td>
-                <td>1</td>
-            </tr>
-        </table>
-    </div> */
