0548df34bafe4dcd27e2b7af6f01032919b2024f
Castillo5115 <adriancastillo51.15@gmail.com>
2022-10-18 18:37:13 +0200

    Añadimos metodos para cambiar las filas y las columnas

--- buscaminas_obj.js
+++ buscaminas_obj.js
@@ -1,38 +1,51 @@
-class Tablero{
-    constructor(filas, columnas){
+class Tablero {
+    constructor(filas, columnas) {
         this.filas = filas;
         this.columnas = columnas;
+
+        this.crearTablero();
     }
 
-    crearTablero(){
+    crearTablero() {
+        // Crear array bidimensional para guardar las minas
         this.arrayTablero = [];
 
         for (let fila = 0; fila < this.filas; fila++) {
-            arrayTablero[fila] = new Array(this.columnas);
-    
+            this.arrayTablero[fila] = [];
+
             for (let columna = 0; columna < this.columnas; columna++) {
-                arrayTablero[fila][columna] = '';
+                this.arrayTablero[fila][columna] = '';
             }
         }
-
     }
 
-    pintarTablero(){
+    dibujarTablero() {
+        // Creamos el tablero en html
         document.write('<table>');
-    
+
         for (let i = 0; i < this.filas; i++) {
             document.write('<tr>');
-        
+
             for (let j = 0; j < this.columnas; j++) {
-                document.write('<td>' + this.arrayTablero[i][j] + '</td>');
+                document.write(`<td>${this.arrayTablero[i][j]}</td>`);
             }
-        
+
             document.write('</tr>');
         }
         document.write('</table>');
     }
 
+    modificarFilas(nuevasFilas){
+        this.filas = nuevasFilas;
+        this.crearTablero();
+    }
+
+    modificarColumnas(nuevasColumnas){
+        this.columnas = nuevasColumnas;
+        this.crearTablero();
+    }
+
 }
 
-const buscaminas = new Tablero(3,4);
-buscaminas.crearTablero();
+const buscaminas = new Tablero(4, 3);
+buscaminas.dibujarTablero();
