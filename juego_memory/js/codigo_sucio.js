067bbaf7913ebd1a00d0a53c703fa6c6f57bfbf8
Castillo5115 <adriancastillo51.15@gmail.com>
2022-10-21 16:39:47 +0200

    Crear la tabla con JS y añadir el array al talero

---
+++ juego_memory/js/codigo_sucio.js
@@ -0,0 +1,25 @@
+let filas = prompt("Numero de filas:");
+let columnas = prompt("Numero de columnas:");
+
+let array = [];
+    
+for (let fila = 0; fila < filas; fila++) {
+    array[fila] = new Array(columnas);
+
+    for (let columna = 0; columna < columnas; columna++) {
+        array[fila][columna] = '';
+    }
+}
+
+document.write('<table>');
+
+        for (let i = 0; i < filas; i++) {
+            document.write('<tr>');
+
+            for (let j = 0; j < columnas; j++) {
+                document.write(`<td>${array[i][j]}</td>`);
+            }
+
+            document.write('</tr>');
+        }
+        document.write('</table>');
