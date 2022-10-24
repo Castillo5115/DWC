dcae9c8c4cc23c28a311748bb04a596427dae0c0
Castillo5115 <adriancastillo51.15@gmail.com>
2022-10-24 17:06:46 +0200

    24.oct.2022 - Cambios generales

--- juego_memory/js/codigo_sucio.js
+++ juego_memory/js/codigo_sucio.js
@@ -1,8 +1,10 @@
 let filas = prompt("Numero de filas:");
 let columnas = prompt("Numero de columnas:");
+let array = []; 
 
-let array = [];
-    
+let posNumFila;
+let posNumColumna;
+
 for (let fila = 0; fila < filas; fila++) {
     array[fila] = new Array(columnas);
 
@@ -13,13 +15,25 @@ for (let fila = 0; fila < filas; fila++) {
 
 document.write('<table>');
 
-        for (let i = 0; i < filas; i++) {
-            document.write('<tr>');
+for (let i = 0; i < filas; i++) {
+    document.write('<tr>');
 
-            for (let j = 0; j < columnas; j++) {
-                document.write(`<td>${array[i][j]}</td>`);
-            }
+    for (let j = 0; j < columnas; j++) {
+        document.write(`<td>${array[i][j]}</td>`);
+    }
 
-            document.write('</tr>');
+    document.write('</tr>');
+}
+document.write('</table>');
+
+for (let i = 0; i < filas; i++) {
+    for (let j = 0; j < columnas; j++) {
+        if (array[i][j] == null) {
+            array[i][j] = 0;
         }
-        document.write('</table>');
+    }
+    
+}
+
+
+
