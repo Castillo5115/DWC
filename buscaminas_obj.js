043be28b1c55d76485d663ccdb9f2d98aa8bf918
Castillo5115 <adriancastillo51.15@gmail.com>
2022-11-08 18:51:48 +0100

    Utilizar DOM

--- buscaminas_obj.js
+++ buscaminas_obj.js
@@ -1,107 +1,110 @@
-class Tablero {
-    constructor(filas, columnas) {
-        this.filas = filas;
-        this.columnas = columnas;
-
-        this.crearTablero();
-    }
-
-    crearTablero() {
-        // Crear array bidimensional para guardar las minas
-        this.arrayTablero = [];
-
-        for (let fila = 0; fila < this.filas; fila++) {
-            this.arrayTablero[fila] = [];
-
-            for (let columna = 0; columna < this.columnas; columna++) {
-                this.arrayTablero[fila][columna] = '';
+document.addEventListener("DOMContentLoaded", function(event){
+    class Tablero {
+        constructor(filas, columnas, ) {
+            this.filas = filas;
+            this.columnas = columnas;
+    
+            this.crearTablero();
+        }
+    
+        crearTablero() {
+            // Crear array bidimensional para guardar las minas
+            this.arrayTablero = [];
+    
+            for (let fila = 0; fila < this.filas; fila++) {
+                this.arrayTablero[fila] = [];
+    
+                for (let columna = 0; columna < this.columnas; columna++) {
+                    this.arrayTablero[fila][columna] = '';
+                }
             }
         }
-    }
-
-    dibujarTablero() {
-        // Creamos el tablero en html
-        document.write('<table>');
-
-        for (let i = 0; i < this.filas; i++) {
-            document.write('<tr>');
-
-            for (let j = 0; j < this.columnas; j++) {
-                document.write(`<td>${this.arrayTablero[i][j]}</td>`);
+    
+        dibujarTablero() {
+            // Creamos el tablero en html
+            let table = document.createElement("table");
+            document.body.appendChild(table);
+            for (let i = 0; i < this.filas; i++) {
+                let tr = document.createElement('tr');
+                table.appendChild(tr);
+                for (let j = 0; j < this.columnas; j++) {
+                   let td = document.createElement('td');
+                   tr.appendChild(td);
+                }
+    
+            
             }
-
-            document.write('</tr>');
         }
-        document.write('</table>');
+    
+        modificarFilas(nuevasFilas) {
+            // Modificar el número de filas y volver a crear el tablero con las filas nuevas
+            this.filas = nuevasFilas;
+    
+            this.crearTablero();
+        }
+    
+        modificarColumnas(nuevasColumnas) {
+            // Modificar el número de columnas y volver a crear el tablero con las columnas nuevas
+            this.columnas = nuevasColumnas;
+    
+            this.crearTablero();
+        }
+    
+    
     }
-
-    modificarFilas(nuevasFilas) {
-        // Modificar el número de filas y volver a crear el tablero con las filas nuevas
-        this.filas = nuevasFilas;
-
-        this.crearTablero();
-    }
-
-    modificarColumnas(nuevasColumnas) {
-        // Modificar el número de columnas y volver a crear el tablero con las columnas nuevas
-        this.columnas = nuevasColumnas;
-
-        this.crearTablero();
-    }
-
-
-}
-
-class Buscaminas extends Tablero {
-    constructor(filas, columnas, numMinas) {
-        super(filas, columnas);
-        this.numMinas = numMinas;
-
-        this.colocarMinas();
-        this.colocarNumMinas();
-    }
-
-    colocarMinas() {
-        let contadorMinas = 0;
-        let posFila;
-        let posColumna;
-
-
-        while (contadorMinas < this.numMinas) {
-            posFila = Math.floor(Math.random() * this.filas);
-            posColumna = Math.floor(Math.random() * this.columnas);
-
-            if (this.arrayTablero[posFila][posColumna] != 'MINA') {
-                this.arrayTablero[posFila][posColumna] = 'MINA';
-                contadorMinas++;
+    
+    class Buscaminas extends Tablero {
+        constructor(filas, columnas, numMinas) {
+            super(filas, columnas);
+            this.numMinas = numMinas;
+    
+            this.colocarMinas();
+            this.colocarNumMinas();
+        }
+    
+        colocarMinas() {
+            let contadorMinas = 0;
+            let posFila;
+            let posColumna;
+    
+    
+            while (contadorMinas < this.numMinas) {
+                posFila = Math.floor(Math.random() * this.filas);
+                posColumna = Math.floor(Math.random() * this.columnas);
+    
+                if (this.arrayTablero[posFila][posColumna] != 'MINA') {
+                    this.arrayTablero[posFila][posColumna] = 'MINA';
+                    contadorMinas++;
+                };
             };
-        };
-    }
-
-    colocarNumMinas() {
-        let numMinasAlrededor;
-
-        for (let fila = 0; fila < this.filas; fila++) {
-            for (let columna = 0; columna < this.columnas; columna++) {
-                numMinasAlrededor = 0;
-                if (this.arrayTablero[fila][columna] != 'MINA') {
-                    for (let cFila = fila - 1; cFila <= fila + 1; cFila++) {
-                        if (cFila >= 0 && cFila < this.filas) {
-                            for (let cColumna = columna - 1; cColumna <= columna + 1; cColumna++) {
-                                if (cColumna >= 0 && cColumna < this.columnas &&
-                                    this.arrayTablero[cFila][cColumna] == 'MINA') {
-                                    numMinasAlrededor++;
+        }
+    
+        colocarNumMinas() {
+            let numMinasAlrededor;
+    
+            for (let fila = 0; fila < this.filas; fila++) {
+                for (let columna = 0; columna < this.columnas; columna++) {
+                    numMinasAlrededor = 0;
+                    if (this.arrayTablero[fila][columna] != 'MINA') {
+                        for (let cFila = fila - 1; cFila <= fila + 1; cFila++) {
+                            if (cFila >= 0 && cFila < this.filas) {
+                                for (let cColumna = columna - 1; cColumna <= columna + 1; cColumna++) {
+                                    if (cColumna >= 0 && cColumna < this.columnas &&
+                                        this.arrayTablero[cFila][cColumna] == 'MINA') {
+                                        numMinasAlrededor++;
+                                    }
                                 }
                             }
+                            this.arrayTablero[fila][columna] = numMinasAlrededor;
                         }
-                        this.arrayTablero[fila][columna] = numMinasAlrededor;
                     }
                 }
             }
         }
     }
-}
+    
+    let buscaminas1 = new Buscaminas(prompt('¿Cuantas filas quieres usar?'), prompt('¿Cuantas columnas quieres?'), prompt('¿Cuantas minas quieres?'));
+    console.log(buscaminas1.arrayTablero);
+    buscaminas1.dibujarTablero();
+});
 
-let buscaminas1 = new Buscaminas(prompt('¿Cuantas filas quieres usar?'), 5, 5);
-console.log(buscaminas1.arrayTablero);
-buscaminas1.dibujarTablero();
