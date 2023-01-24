2f18c083a3ce89cf7e9aab634fb83231a295ebd5
Castillo5115 <adriancastillo51.15@gmail.com>
2023-01-24 19:11:05 +0100

    Validar email y apellidos 2023-01-24

--- U.3/Proy_ValidacionDNI/js/validarNIF.js
+++ U.3/Proy_ValidacionDNI/js/validarNIF.js
@@ -3,11 +3,30 @@ function validarNombre(){
 
     let esValido = patron.test(`AA`);
     console.log(esValido);
-    esValido = patron.test(`s`);
-    esValido
 }
 
-document.addEventListener('load', function(){
+function validarApellidos(){
+    let patron = /^[A-Za-záéíóúüïàèìòùÁÉÍÓÚñÑçÇ]{2,}$/;
+
+    let esValido = patron.test(`AA`);
+    console.log(esValido);
+}
+
+function validarEmail(){
+    let patron = /^.+@.+$/;
+
+    this.className = "";
+    if (patron.test(this,value)) {
+        this.className = "verde";
+    }
+}
+
+window.addEventListener('load', function(){
     let nombre = document.getElementById('nombre');
+    let apellidos = document.getElementById('apellidos');
+    let email = document.getElementById('email');
+
     nombre.addEventListener('keyup', validarNombre);
+    apellidos.addEventListener('keyup', validarApellidos);
+    email.addEventListener('keyup', validarEmail);
 });
