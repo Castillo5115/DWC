fcd5aa9a80e995f405bffaa323b53720e70bd357
Castillo5115 <adriancastillo51.15@gmail.com>
2023-02-06 17:55:15 +0100

    act_14_mapas

---
+++ U.3/Act_14_Mapas/act_14_mapas.js
@@ -0,0 +1,16 @@
+const registroPacientes = new Map([
+    [`AAA024`, `Fernandez M. (321790059) -> C/Recoletos, 50`],
+    [`BCD827`, `Ruíz P. (100973253) -> C/Esquerdo izquierdo, 103`],
+    [`YUN385`, `Benítez E. (154811767) -> Av. Argentina, 5`]
+]);
+let numeroRegistro = "";
+console.log('{');
+for (const x of registroPacientes.keys()) {
+    console.log('Paciente ',i+1,'=>');
+    console.log('`numeroRegistro: ', x , ',');
+    console.log('nombreCompleto: ,');
+    console.log('numeroSS: ,');
+    console.log('direccion: ´');
+}
+console.log('}');
+
