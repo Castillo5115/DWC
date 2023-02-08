de54cc4e6be63a96a19cd6ba111ec9577eb34e72
Castillo5115 <adriancastillo51.15@gmail.com>
2023-02-08 16:16:48 +0100

    actividad de mapas 20223-02-08

--- U.3/Act_14_Mapas/act_14_mapas.js
+++ U.3/Act_14_Mapas/act_14_mapas.js
@@ -3,14 +3,9 @@ const registroPacientes = new Map([
     [`BCD827`, `Ruíz P. (100973253) -> C/Esquerdo izquierdo, 103`],
     [`YUN385`, `Benítez E. (154811767) -> Av. Argentina, 5`]
 ]);
-let numeroRegistro = "";
-console.log('{');
-for (const x of registroPacientes.keys()) {
-    console.log('Paciente ',i+1,'=>');
-    console.log('`numeroRegistro: ', x , ',');
-    console.log('nombreCompleto: ,');
-    console.log('numeroSS: ,');
-    console.log('direccion: ´');
-}
-console.log('}');
 
+
+for(let [clave,valor] of registroPacientes){
+    console.log(clave);
+    console.log(valor);
+}
